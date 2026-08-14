37f9b798e740eb5268659470c699ad8c64fdea5a
junok1m <o489212169@gmail.com>
2026-08-14 19:54:47 +1000

    feat: add permanent redirects from /girls/* to main routes

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,60 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/girls",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/girls/jobs",
+        destination: "/jobs",
+        permanent: true,
+      },
+      {
+        source: "/girls/qna",
+        destination: "/qna",
+        permanent: true,
+      },
+      {
+        source: "/girls/reviews",
+        destination: "/reviews",
+        permanent: true,
+      },
+      {
+        source: "/girls/blog",
+        destination: "/blog",
+        permanent: true,
+      },
+      {
+        source: "/girls/jobs/:slug*",
+        destination: "/jobs/:slug*",
+        permanent: true,
+      },
+      {
+        source: "/girls/qna/:slug*",
+        destination: "/qna/:slug*",
+        permanent: true,
+      },
+      {
+        source: "/girls/reviews/:slug*",
+        destination: "/reviews/:slug*",
+        permanent: true,
+      },
+      {
+        source: "/girls/blog/:slug*",
+        destination: "/blog/:slug*",
+        permanent: true,
+      },
+      {
+        source: "/girls/:path*",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
